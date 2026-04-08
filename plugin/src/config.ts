ed325b55a73850799bb0348be4973befce61ac31
Rohid Dev <rohid@rohiddev.com>
2026-04-07 17:20:39 -0700

    fix: correct Harness account ID to N2EzMWI4YjQtNDU4OS00Zj
    Co-Authored-By: Rohid Dev <rohid@rohiddev.com>

--- plugin/src/config.ts
+++ plugin/src/config.ts
@@ -2,7 +2,7 @@
 // Update HARNESS_HOST and HARNESS_ACCOUNT_ID when deploying to a new environment.
 
 export const HARNESS_HOST = 'citizensbankdev.harness.io';
-export const HARNESS_ACCOUNT_ID = 'N2EzMWI4YjQ1NDU4OS0Zj';
+export const HARNESS_ACCOUNT_ID = 'N2EzMWI4YjQtNDU4OS00Zj';
 
 export const IDP_BASE = `https://${HARNESS_HOST}/ng/account/${HARNESS_ACCOUNT_ID}/module/idp`;
 
